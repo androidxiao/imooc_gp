033823df42de9b970bb1a98b445c601526c105ba
androidxiao <zhawei1993@163.com>
2018-03-14 23:15:33 +0800

    使用Navigator

---
+++ Girl.js
@@ -0,0 +1,43 @@
+/**
+ * Created by chawei on 2018/3/14.
+ */
+import React, {Component} from 'react'
+import {
+    View,
+    Text,
+StyleSheet
+}from 'react-native';
+
+export default class Girl extends Component{
+    constructor(props){
+        super(props);
+        this.state=({
+                
+        })
+    }
+    render(){
+        return(
+            <View style={styles.container}>
+            <Text style={styles.text}>I am Girl</Text>
+            <Text style={styles.text}>我收到了男孩的：{this.props.word}</Text>
+            <Text style={styles.text}
+            onPress={()=>{
+                this.props.onCallBack('一盒巧克力')
+                this.props.navigator.pop()
+            }}>回赠巧克力</Text>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'red',
+        justifyContent:'center',
+    },
+    text:{
+        fontSize:22,
+    
+    }
+})
